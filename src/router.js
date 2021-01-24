88914e98188a016a25f23d5228c5f9430dc0bd28
oat1314 <403963470@qq.com>
2021-01-24 23:59:23 +0800

    first commit

--- src/router.js
+++ src/router.js
@@ -1,25 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './views/Login'
+import Admin from './views/Admin'
+import Welcome from './views/Admin/Welcome'
+import BookDetial from './views/Admin/BookDetail'
+import BookModify from './views/Admin/BookModify'
+import BookSort from './views/Admin/BookSort'
+import User from './views/Admin/User'
+import UserMessage from './views/Admin/UserMessage'
+
+const routes = [
+  {path: '',redirect: '/login'},
+  {path:'/login',component:Login},
+  {path:'/admin',name: "admin",component:Admin,
+    children: [
+      {path:'/book/welcome',name: 'welcome',component:Welcome},
+      {path:'/book/detail',name: 'welcome',component:BookDetial},
+      {path:'/book/modify',name: 'welcome',component:BookModify},
+      {path:'/book/sort',name: 'welcome',component:BookSort},
+      {path:'/book/user',name: 'welcome',component:User},
+      {path:'/book/usermessage',name: 'welcome',component:UserMessage},
+  ]
+},
+]
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+  routes: routes
 })
---
+++ src/views/Admin/Welcome/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        欢迎进入图书管理系统
+    </div>
+</template>
